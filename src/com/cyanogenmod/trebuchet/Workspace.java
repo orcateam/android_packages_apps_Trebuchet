5936b81bc651b4ca0f69c99c4a55c1fcc30c55be
Romain Guy <romainguy@google.com>
2013-02-25 15:00:36 -0800

    Disable clipping where unnecessary
    This change prevents the renderer from using a stencil buffer
    to render rotated CellLayouts.
    
    Change-Id: I0414f59ed45502323ab588820ceb4a5e3ec17140

--- src/com/cyanogenmod/trebuchet/Workspace.java
+++ src/com/cyanogenmod/trebuchet/Workspace.java
@@ -526,6 +526,8 @@ public class Workspace extends PagedView
         LauncherApplication app = (LauncherApplication)context.getApplicationContext();
         mIconCache = app.getIconCache();
         setWillNotDraw(false);
+        setClipChildren(false);
+        setClipToPadding(false);
         setChildrenDrawnWithCacheEnabled(true);
 
         final Resources res = getResources();
