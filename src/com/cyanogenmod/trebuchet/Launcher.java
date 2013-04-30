698b88129161afdec8935048f22038a453b3c107
Jorge Ruesga <jorge@ruesga.com>
2013-05-01 00:49:36 +0200

    Trebuchet: Use preference provider to lock/unlock homescreen
    mSharedPreferences uses "com.cyanogenmod.trebuchet.prefs", while the Preferences activity uses
    "com.cyanogenmod.trebuchet_preferences". Use PreferenceProvider to sync lock/unlock homescreen.
    
    Change-Id: Ib9312a9f8cffe3f009b8cd8d9b9789d459c1bd96
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/cyanogenmod/trebuchet/Launcher.java
+++ src/com/cyanogenmod/trebuchet/Launcher.java
@@ -1883,9 +1883,7 @@ public final class Launcher extends Activity
                 return true;
             case MENU_LOCK_WORKSPACE:
                 mLockWorkspace = !mLockWorkspace;
-                SharedPreferences.Editor editor = mSharedPrefs.edit();
-                editor.putBoolean("ui_general_lock_workspace", mLockWorkspace);
-                editor.commit();
+                PreferencesProvider.Interface.General.setLockWorkspace(this, mLockWorkspace);
                 return true;
         }
 
